3435742916d84117b21959ef0fb740bdeb3816c6
Miriam Ewall-Wice <121811251+mirew4@users.noreply.github.com>
2026-03-10 17:55:11 -0700

    Delete drafts directory

--- drafts/Python4Hackers_1.py
+++
@@ -1,3 +0,0 @@
-# Exercise 1 in Python For Hackers FULL Course by Ryan John
-# Take first name input, take last name input, print "Your name is "
-print("Your name is " + input("What is your first name?") + " " + input("What is your last name? "))
